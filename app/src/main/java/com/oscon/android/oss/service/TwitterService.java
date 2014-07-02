4c6bd0ae6d8d901c05d22ac6e5b34275b171b8b4
Mohammad Almalkawi <mohammad.almalkawi@gmail.com>
2014-07-02 00:42:57 -0700

    Format searchTweets

--- app/src/main/java/com/oscon/android/oss/service/TwitterService.java
+++ app/src/main/java/com/oscon/android/oss/service/TwitterService.java
@@ -8,5 +8,7 @@ public interface TwitterService {
     public static final String AUTH_HEADER = "Bearer AAAAAAAAAAAAAAAAAAAAALKiYQAAAAAAY5Zy%2FKmPBYOh8by7IKAFMi5mAbI%3DWyeNjQmXcSmtuRYT15f7MmHK10WjHh1VVGU02IIMhy1bwtfglB";
 
     @GET("/search/tweets.json")
-    SearchResults searchTweets(@Query("q") String query);
+    SearchResults searchTweets(
+            @Query("q") String query
+    );
 }
